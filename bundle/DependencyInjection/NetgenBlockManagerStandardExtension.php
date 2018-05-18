37029aafa27067ac32ae786da1b7aa7ea2362988
Edi Modrić <edi.modric@gmail.com>
2018-05-18 13:40:27 +0200

    Make sure Yaml::parse receives a string

--- bundle/DependencyInjection/NetgenBlockManagerStandardExtension.php
+++ bundle/DependencyInjection/NetgenBlockManagerStandardExtension.php
@@ -44,7 +44,7 @@ final class NetgenBlockManagerStandardExtension extends Extension implements Pre
 
         foreach ($prependConfigs as $configFile => $prependConfig) {
             $configFile = __DIR__ . '/../Resources/config/' . $configFile;
-            $config = Yaml::parse(file_get_contents($configFile));
+            $config = Yaml::parse((string) file_get_contents($configFile));
             $container->prependExtensionConfig($prependConfig, $config);
             $container->addResource(new FileResource($configFile));
         }
