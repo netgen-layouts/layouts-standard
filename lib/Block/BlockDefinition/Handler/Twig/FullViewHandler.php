926cfdd0d6f8726da8697f702fb85d97c7f6c61d
Edi Modrić <edi.modric@gmail.com>
2019-07-04 16:47:06 +0200

    Update phpdoc

--- lib/Block/BlockDefinition/Handler/Twig/FullViewHandler.php
+++ lib/Block/BlockDefinition/Handler/Twig/FullViewHandler.php
@@ -11,7 +11,7 @@ use Netgen\Layouts\Block\BlockDefinition\TwigBlockDefinitionHandlerInterface;
 /**
  * Block used to render the full view (or rather, the result of the controller).
  *
- * The default block name to use & render is provided by the constructor.
+ * The default block names to use & render are provided by the constructor.
  */
 final class FullViewHandler extends BlockDefinitionHandler implements TwigBlockDefinitionHandlerInterface
 {
